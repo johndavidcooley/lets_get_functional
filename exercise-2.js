c7cdb9cc989b8f820720ab165d1d505fdefc1781
John Cooley <johndavidcooley@gmail.com>
2016-03-03 00:39:27 -0500

    Tried my best to complete all of the questions.
    I came short on three of them, but I think I'm remotely close on two of those three.

--- exercise-2.js
+++ exercise-2.js
@@ -49,8 +49,38 @@ function isVowel(char){
 // ---------------------
 
 function rovarspraket(phrase){
-    //...
+    //Tried my best on this one. I think I am somewhat close, but missing a key instruction that will likely seem easy after I see it explained.
+    var consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+    var stringToArray = phrase.split('');
+    var rovarArray = stringToArray.forEach(function(consonants, currentValue) {
+    	if(currentValue === consonant[0] || currentValue === consonant[1] || currentValue === consonant[2] || currentValue === consonant[3] || currentValue === consonant[4] || currentValue === consonant[5] || currentValue === consonant[6] || currentValue === consonant[7] || currentValue === consonant[8] || currentValue === consonant[9] || currentValue === consonant[10] || currentValue === consonant[11] || currentValue === consonant[12] ||currentValue === consonant[13] ||currentValue === consonant[14] ||currentValue === consonant[15] ||currentValue === consonant[16] ||currentValue === consonant[17] ||currentValue === consonant[18] ||currentValue === consonant[19] ||currentValue === consonant[20]) {
+    	return currentValue + 'o' + currentValue;
+    } else {
+    	return currentValue;
+    }
+    var rovarString = rovarArray.join('');
+    });
+    return rovarString;
+};
+
+function rovarspraket(phrase){
+    //
+    var consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+    // var stringToArray = phrase.split('');
+    var rovarArray = phrase.forEach(function(consonants, currentValue) {
+    	if(currentValue === consonant[0] || currentValue === consonant[1] || currentValue === consonant[2] || currentValue === consonant[3] || currentValue === consonant[4] || currentValue === consonant[5] || currentValue === consonant[6] || currentValue === consonant[7] || currentValue === consonant[8] || currentValue === consonant[9] || currentValue === consonant[10] || currentValue === consonant[11] || currentValue === consonant[12] ||currentValue === consonant[13] ||currentValue === consonant[14] ||currentValue === consonant[15] ||currentValue === consonant[16] ||currentValue === consonant[17] ||currentValue === consonant[18] ||currentValue === consonant[19] ||currentValue === consonant[20]) {
+    	return currentValue + 'o' + currentValue;
+    } else {
+    	return currentValue;
+    };
+    // var roverString = roverArray.join('');
+    // return rovarString;
+});
+    return rovarArray;
 }
+
+
+
 
 
 // ---------------------
@@ -69,6 +99,26 @@ function reverse(string){
 // Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
 // ---------------------
 
+function findLongestWord(words){
+	// ['alpa', 'beta', 'charlie', 'delta', 'onomatopoeia'] is a ample array I used to test the function above.
+	//Take the array of "words". Turn it into a new array consisting of the length of each index.
+	//Use Math.max() to calculate the longest word in the set. Identify the position of that value in the array.
+	//Return the same position for the "words" array to determine the longest word.
+	var wordsToNumbers = words.map(function(currentWord) {
+		numArray = currentWord.length;
+		return numArray;
+	});
+	var highestNumber = Math.max.apply(Math,numArray);
+	var indexOfHighestNumber = ?????
+	return words[indexOfHighestNumber];
+};
+
+
+
+// ---------------------
+// Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
+// ---------------------
+
 function filterLongWords(words, i){
 	var longWords = words.filter(function(currentWord) {
 		if(currentWord.length >= i) {
@@ -81,21 +131,9 @@ function filterLongWords(words, i){
 };
 
 // ---------------------
-// Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
-// ---------------------
-
-function filterLongWords(words, i){
-//     var wordList = ["a", "be", "can", "dill", "every", "friend", "glamour", "Hawaiian", "Indonesia", "JavaScript"];
-//     var longWords = filter.words(function(currentWord) {
-//     	var isLong = currentWord >= i;
-//     	return isLong;
-//     })
-// };
-
-// ---------------------
 // Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
 // ---------------------
 
 function charFreq(string){
-    //...
+	//I have no idea on this one.
 }
